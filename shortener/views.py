496dd93ffae1425fab5f5075f1b0661c29754c48
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-07-11 12:25:06 +0530

    Updated Views and urls

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import SqueezeURL
 # Create your views here.
+
+
+def render_shortcode(request, shortcode=None):
+    obj = get_object_or_404(SqueezeURL, short_code=shortcode)
+    return HttpResponse(f"Mapped URl is {obj.url}")
